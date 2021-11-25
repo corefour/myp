a71a9c9bea5082fdb2fc80b121c9ec16e1ee6692
Yash Kamtekar <Yash.Kamtekar@sjsu.edu>
2021-11-24 16:42:48 -0800

    set up for amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "project2423fe81": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}

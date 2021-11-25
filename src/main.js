f488e78a8dc4c5e243c1c4d787cbc039e4bda1f8
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2021-11-24 17:21:15 -0800

    add auth ui and functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Authenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 
-export default function Main() {
+Amplify.configure(awsconfig);
+
+export default function Main(){
+
+
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
     )
 }

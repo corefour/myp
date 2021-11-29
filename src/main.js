bbd02190fdd1623c923c94a6738d79ca32addab4
Yash-Kamtekar <yash.kamtekar@sjsu.edu>
2021-11-28 17:11:25 -0800

    ui structure updated

--- src/main.js
+++ src/main.js
@@ -3,39 +3,49 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
+
 import { HashRouter } from "react-router-dom";
-import {lastIndexOf, substr} from '@7urtle/lambda';
+import { lastIndexOf, substr } from '@7urtle/lambda';
 import Router from './Routes';
+
+
+import { ChakraProvider } from "@chakra-ui/react";
+// import Auth from "./pages/main";
+// import Topbar from "./common/layout/topbar"
 
 Amplify.configure(awsconfig);
 
 export default function Main() {
 
-  /**
- * Turns URL path into router basename by removing everything after the last slash
- * 
- * @RajKinkhabwala getBasename :: string -> string
- *
- * @pure
- * @param {string} path URL path, probably window.location.pathname
- * @returns {string} final basename
- * 
- * @example
- * getBaseName('/some/structure/page'); // => '/some/structure'
- */
-const getBasename = path => substr(lastIndexOf('/')(path))(0)(path);
+    /**
+   * Turns URL path into router basename by removing everything after the last slash
+   * 
+   * @RajKinkhabwala getBasename :: string -> string
+   *
+   * @pure
+   * @param {string} path URL path, probably window.location.pathname
+   * @returns {string} final basename
+   * 
+   * @example
+   * getBaseName('/some/structure/page'); // => '/some/structure'
+   */
+    const getBasename = path => substr(lastIndexOf('/')(path))(0)(path);
 
-  return (
-    <Authenticator initialState="signUp" variation="modal">
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <HashRouter basename={getBasename(window.location.pathname)}>
-            <Router />
-          </HashRouter>
-        </main>
-      )}
-    </Authenticator>
-  )
+    return (
+        <ChakraProvider>
+            <Authenticator initialState="signUp" variation="modal">
+                {({ signOut, user }) => (
+                    <main>
+                        <h1>Hello {user.username}</h1>
+                        <button onClick={signOut}>Sign out</button>
+                        <HashRouter basename={getBasename(window.location.pathname)}>
+                            <Router />
+                        </HashRouter>
+                    </main>
+                )}
+            </Authenticator>
+            {/* <Auth /> */}
+            {/* <Topbar /> */}
+        </ChakraProvider>
+    )
 }

f1b5ec78213b50580d28863bcef22b021ef026f8
shivanipandit88 <89808009+shivanipandit88@users.noreply.github.com>
2021-12-02 03:10:30 -0800

    Company Profile UI

--- src/pages/Company/components/companyprofile.js
+++ src/pages/Company/components/companyprofile.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { 
+    Tabs,
+    TabList,
+    TabPanels,
+    Tab,
+    TabPanel,
+    Box,
+    Container,
+    Image,
+    Text
+} from '@chakra-ui/react';
+import logo from "../../../assets/company-logo.png";
+import bg from "../../../assets/bg.jpg";
 
 export default function CompanyProfile(){
 
     return (
-        <div>
-            hello
-        </div>
+        <Box className="company-profile">
+            <Box className="banner">
+                <Image src={bg} alt="Banner" className="bannerImg" />
+                <Container maxW='container.xl'>
+                    <Box className="company-title">
+                        <Image src={logo} alt="Company Logo" />
+                        <Text>Company Name</Text>
+                    </Box>
+                </Container>
+            </Box>
+            <Box className="tabs">
+                <Container maxW='container.xl'>
+                    <Tabs>
+                        <TabList>
+                            <Tab>Products</Tab>
+                            <Tab>Sales</Tab>
+                            <Tab>Expenses</Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                            <p>one!</p>
+                            </TabPanel>
+                            <TabPanel>
+                            <p>two!</p>
+                            </TabPanel>
+                            <TabPanel>
+                            <p>three!</p>
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </Container>
+            </Box>
+        </Box>
     )
 }

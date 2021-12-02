64e135e7cbadd0ac231a12040bc51683c20adaf4
Yash-Kamtekar <Yash.Kamtekar@sjsu.edu>
2021-12-01 18:30:02 -0800

    get company list api integrated and component created for it

--- src/Router.js
+++ src/Router.js
@@ -15,11 +15,12 @@ import Products from "./pages/Products/index"
 const Router = () => (
     <Routes>
         <Route path='/' element={<Home />} />
+
         <Route path='/users/*' element={<Users />} />
         <Route path='/users/create' exact element={<UserCreate />}/>
+
         <Route path='/company/*' element={<Company />} />
         <Route path='/company/create' exact element={<CompanyCreate />}/>
-        <Route path='/users/create' exact element={<UserCreate />} />
 
         <Route path='/products' exact element={<Products />} />
         {/* Do not remove this route its for 404 */}
--- src/pages/Company/index.js
+++ src/pages/Company/index.js
@@ -2,40 +2,27 @@ import React, { useState, useEffect } from "react";
 import {
     Box,
     Container,
-    Button,
-    Grid,
-    Text
+    Button
 } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import {useNavigate, Outlet} from 'react-router-dom';
+
+import Companylist from "./components/listcompanys"
+import { allCompanys } from "../../services/Company";
+import { useNavigate, Outlet } from 'react-router-dom';
 
 function User() {
     let navigate = useNavigate();
+    const [companys, setCompanys] = useState([])
 
-
+    useEffect(() => { allCompanys().then((res) => setCompanys(res.data.listCompanys.items)) }, [])
 
     return (
         <Box mt="40px" className="company">
             <Container maxW='container.xl'>
                 <Button onClick={() => navigate('create')} float="right" bgColor="pink.500" color="blue.50" mb="30px" _hover={{ bg: "pink.700" }}>Add Company</Button>
-                <Box className="clear"></Box>
-                <Grid templateColumns='repeat(4, 1fr)' gap={30} textAlign="center" lineHeight="70px" fontSize="18" className="companyList">
-                    <Link to="/">
-                        <Text>Company Name</Text>
-                    </Link>
-                    <Link to="/">
-                        <Text>Company Name</Text>
-                    </Link>
-                    <Link to="/">
-                        <Text>Company Name</Text>
-                    </Link>
-                    <Link to="/">
-                        <Text>Company Name</Text>
-                    </Link>
-                    <Link to="/" w='100%' h='70' bg='gray.100' borderRadius="5" _hover={{ bg: "gray.500", color: "gray.100", boxShadow: "lg" }} transition="all" transitionDuration="0.4s">
-                        <Text>Company Name</Text>
-                    </Link>
-                </Grid>
+                <Box className="clear">
+                    <Companylist companys={companys} />
+                </Box>
+
             </Container>
             {/* <Outlet /> */}
         </Box>

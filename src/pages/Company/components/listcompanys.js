98d4532677335d4ff04d820a8d4d2c12535aa1eb
Yash-Kamtekar <Yash.Kamtekar@sjsu.edu>
2021-12-02 02:36:20 -0800

    company delete api inegrated and states managed for adding and deleting companys

--- src/pages/Company/components/listcompanys.js
+++ src/pages/Company/components/listcompanys.js
@@ -1,21 +1,35 @@
 import React from "react";
 import {
     Grid,
-    Text
+    Text,
+    Box
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { Button } from "@aws-amplify/ui-react";
 import { FaRegTrashAlt } from 'react-icons/fa';
+import { removeCompany } from "../../../services/Company"
 
 function Companylist(props) {
+
+    function deleteCompany(id, index) {
+        removeCompany(id)
+        let temp = [...props.companys]
+        temp.splice(index, 1)
+        props.setCompanys(temp)
+    }
+
     return (
         <Grid templateColumns='repeat(4, 1fr)' gap={30} textAlign="center" lineHeight="70px" fontSize="18" className="companyList">
             {
-                props.companys.map((item) => (
-                    <Link to={"/company/" + item.id}>
-                        <Text>{item.name}</Text>
-                        <Button className="deleteIcon"><FaRegTrashAlt /></Button>
-                    </Link>
+                props.companys.map((item, index) => (
+                    <>
+                        <Box className="indiCompany">
+                            <Button className="deleteIcon" onClick={() => deleteCompany(item.id, index)}><FaRegTrashAlt /></Button>
+                            <Link to={"/company/" + item.id}>
+                                <Text>{item.name}</Text>
+                            </Link>
+                        </Box>
+                    </>
                 ))
             }
 

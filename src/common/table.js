27a7d1555ba5b1cda97909eead511c128663b0de
Yash-Kamtekar <Yash.Kamtekar@sjsu.edu>
2021-12-03 21:49:26 -0800

    tabulator added

---
+++ src/common/table.js
@@ -0,0 +1,16 @@
+/* Required columns , data , ref*/
+
+import React from 'react'
+import { ReactTabulator } from 'react-tabulator'
+
+export default function Table(props) {
+	return (
+		<ReactTabulator
+			columns={props.columns}
+			data={props.tabledata}
+			ref={props.innerRef}
+			// options={props.options}
+			rowClick={props.rowClick ? props.rowClick : () => {}}
+		/>
+	)
+}

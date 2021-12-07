cce7c03879f07288488171563d047277a272843a
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2021-12-07 01:30:00 -0800

    error fixed

--- src/services/user.js
+++ src/services/user.js
@@ -15,7 +15,7 @@ export async function getCurrentUserRole() {
 
     try {
         const { idToken } = await getCurrentUserJwtPayload();
-        if (idToken.hasOwnProperty('cognito:groups')){
+        if (idToken.payload.hasOwnProperty('cognito:groups')){
             const role = idToken.payload["cognito:groups"][0];
             return role;
         }
